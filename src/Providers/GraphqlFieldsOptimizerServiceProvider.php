e7c86977e24b806018214033ea9411622fbd3c38
Antoine Corbin <antoine.corbin@big-boss-studio.com>
2024-04-24 20:52:19 +0200

    chore: create service provider

--- src/Providers/GraphqlFieldsOptimizerServiceProvider.php
+++ src/Providers/GraphqlFieldsOptimizerServiceProvider.php
@@ -1,6 +1,20 @@
 <?php
 
-class GraphqlFieldsOptimizerServiceProvider
-{
+namespace BBSLab\GraphqlFieldsOptimizer\Providers;
 
+use Illuminate\Support\ServiceProvider;
+
+class GraphqlFieldsOptimizerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/graphql-fields-optimizer.php' => config_path('graphql-fields-optimizer.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../../config/graphql-fields-optimizer.php', 'graphql-fields-optimizer');
+    }
 }
